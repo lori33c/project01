72de5e4789802cba379e43b9e8c8af577ef157ff
Lori Cha <lcha@csu.fullerton.edu>
2022-03-03 21:03:13 +0000

    Fixed Syntax

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 # include <vector>
 # include "cpputils/graphics/image.h"
 
-void DrawPlayer() {
+void DrawPlayer(std::string player_image_file) {
   // 1. Create Player image (50 x 50 pixel)
   const int player_size = 50;
   graphics::Image player_image(player_size, player_size);
@@ -24,10 +24,10 @@ void DrawPlayer() {
   player_image.DrawCircle(25, 25 , 1, 0, 0, 0);
 
   // Saves the Player image
-  player_image.SaveImageBmp("player.bmp");
+  player_image.SaveImageBmp(player_image_file);
 }
 
-void DrawOpponent() {
+void DrawOpponent(std::string opponent_image_file) {
   // 2. Create Opponent image (50 x 50 pixel)
   // 1. Create Opponent image (50 x 50 pixel)
   const int Opponent_size = 50;
@@ -45,25 +45,24 @@ void DrawOpponent() {
   // Draw mouth
   Opponent_image.DrawPolygon({25, 25, 20, 32, 30, 32}, 243, 0, 0);
 
-
   // Saves the Player image
-  Opponent_image.SaveImageBmp("opponent.bmp");
+  Opponent_image.SaveImageBmp(opponent_image_file);
 }
 
-void DrawPlayerProjectile() {
+void DrawPlayerProjectile(std::string player_projectile_file) {
   // 3. Create Player Projectile image (5 x 5 pixel)
   const int play_size = 5;
 	graphics::Image play_image(play_size, play_size);
 	play_image.DrawCircle(play_size/2, play_size/2, play_size/2, 0, 0, 255);
-	play_image.SaveImageBmp("PlayerProjectile.bmp");
+	play_image.SaveImageBmp(player_projectile_file);
 }
 
-void DrawOpponentProjectile() {
+void DrawOpponentProjectile(std::string opponent_projectile_file) {
   // 4. Create Opponent Projectile image (5 x 5 pixel)
   const int Opp_size = 5;
 	graphics::Image Opp_image(Opp_size, Opp_size);
 	Opp_image.DrawCircle(Opp_size/2, Opp_size/2, Opp_size/2, 255, 0, 0);
-	Opp_image.SaveImageBmp("OpponnetProjectile.bmp");
+	Opp_image.SaveImageBmp(opponent_projectile_file);
 }
 
 int main() {
@@ -73,28 +72,20 @@ int main() {
   std::string player_projectile_file;
   std::string opponent_projectile_file;
 
-  // Create Player image
-  DrawPlayer();
-  
-  // 2. Create Opponent image (50 x 50 pixel)
-  DrawOpponent();
-
-  // 3. Create Player Projectile image (5 x 5 pixel)
-  DrawPlayerProjectile();
-
-  // 4. Create Opponent Projectile image (5 x 5 pixel)
-  DrawOpponentProjectile();
-
   // Prompt user to input file names
   std::cout << "Please provide player image filename: ";
   std::cin >> player_image_file;
   std::cout << "Please provide opponent image filename: ";
   std::cin >> opponent_image_file;
-  std::cout << "Please provide player projectile filename: ";
+  std::cout << "Please provide player projectile image filename: ";
   std::cin >> player_projectile_file;
-  std::cout << "Please provide opponent projectile filename: ";
+  std::cout << "Please provide opponent projectile image filename: ";
   std::cin >> opponent_projectile_file;
 
+  DrawPlayer(player_image_file);
+  DrawOpponent(opponent_image_file);
+  DrawPlayerProjectile(player_projectile_file);
+  DrawOpponentProjectile(opponent_projectile_file);
 
   return 0;
 }
--- reflection.md
+++ reflection.md
@@ -3,8 +3,14 @@
 ## Description
 Describe what you did for this milestone in your own words.
 
+In this milestone, we created 4 images: player, opponent, opponent projectile, player projectile.
+
 ## Challenges encountered
 Describe the challenges you encountered while working on this milestone of the project.
 
+It was difficult to make the images at first, because I didn't know about the coordinate system.
+
 ## Things I've learned
 What is the most important thing you've learned from this milestone in the project?
+
+We learned how to utilize graphics package to create images
