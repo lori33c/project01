bfb33dade8ce47063eb835f51be34e92aa0e80e3
Lori Cha <lcha@csu.fullerton.edu>
2022-03-03 21:12:10 +0000

    Fixed Format

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
-# include <iostream>
-# include <string>
-# include <vector>
-# include "cpputils/graphics/image.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include "cpputils/graphics/image.h"
 
 void DrawPlayer(std::string player_image_file) {
   // 1. Create Player image (50 x 50 pixel)
@@ -9,7 +9,8 @@ void DrawPlayer(std::string player_image_file) {
   graphics::Image player_image(player_size, player_size);
 
   // Draws the face
-  player_image.DrawCircle(player_size/2, player_size/2, player_size/3, 167, 133, 106);
+  player_image.DrawCircle(player_size / 2, player_size / 2, player_size / 3,
+                          167, 133, 106);
   player_image.DrawPolygon({25, 20, 10, 5, 10, 30}, 167, 133, 106);
   player_image.DrawPolygon({25, 20, 40, 5, 40, 30}, 167, 133, 106);
 
@@ -21,7 +22,7 @@ void DrawPlayer(std::string player_image_file) {
   player_image.DrawPolygon({25, 25, 20, 32, 30, 32}, 243, 82, 120);
 
   // Draw nose
-  player_image.DrawCircle(25, 25 , 1, 0, 0, 0);
+  player_image.DrawCircle(25, 25, 1, 0, 0, 0);
 
   // Saves the Player image
   player_image.SaveImageBmp(player_image_file);
@@ -34,7 +35,8 @@ void DrawOpponent(std::string opponent_image_file) {
   graphics::Image Opponent_image(Opponent_size, Opponent_size);
 
   // Draws the face
-  Opponent_image.DrawCircle(Opponent_size/2, Opponent_size/2, Opponent_size/3, 0, 0, 0);
+  Opponent_image.DrawCircle(Opponent_size / 2, Opponent_size / 2,
+                            Opponent_size / 3, 0, 0, 0);
   Opponent_image.DrawPolygon({25, 20, 10, 5, 10, 30}, 0, 0, 0);
   Opponent_image.DrawPolygon({25, 20, 40, 5, 40, 30}, 0, 0, 0);
 
@@ -52,17 +54,17 @@ void DrawOpponent(std::string opponent_image_file) {
 void DrawPlayerProjectile(std::string player_projectile_file) {
   // 3. Create Player Projectile image (5 x 5 pixel)
   const int play_size = 5;
-	graphics::Image play_image(play_size, play_size);
-	play_image.DrawCircle(play_size/2, play_size/2, play_size/2, 0, 0, 255);
-	play_image.SaveImageBmp(player_projectile_file);
+  graphics::Image play_image(play_size, play_size);
+  play_image.DrawCircle(play_size / 2, play_size / 2, play_size / 2, 0, 0, 255);
+  play_image.SaveImageBmp(player_projectile_file);
 }
 
 void DrawOpponentProjectile(std::string opponent_projectile_file) {
   // 4. Create Opponent Projectile image (5 x 5 pixel)
   const int Opp_size = 5;
-	graphics::Image Opp_image(Opp_size, Opp_size);
-	Opp_image.DrawCircle(Opp_size/2, Opp_size/2, Opp_size/2, 255, 0, 0);
-	Opp_image.SaveImageBmp(opponent_projectile_file);
+  graphics::Image Opp_image(Opp_size, Opp_size);
+  Opp_image.DrawCircle(Opp_size / 2, Opp_size / 2, Opp_size / 2, 255, 0, 0);
+  Opp_image.SaveImageBmp(opponent_projectile_file);
 }
 
 int main() {
